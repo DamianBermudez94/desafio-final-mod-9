0e682ebf855abe3064a8a209a0f7abaabbf02a45
Damian Bermudez <bermudezdamian7gmail.com>
2022-12-05 17:54:20 -0300

    agregando configuracion de cors

--- pages/api/_middlewere.ts
+++ pages/api/_middlewere.ts
@@ -8,15 +8,12 @@ export function middleware(req: NextRequest) {
         "Access-Control-Allow-Credentials": "true",
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-        "Access-Control-Allow-Headers":
-          req.headers.get("Access-Control-Request-Headers"),
+        "Access-Control-Allow-Headers": req.headers.get(
+          "Access-Control-Request-Headers"
+        ),
         Vary: "Access-Control-Request-Headers",
         "Content-Length": "0",
       },
     });
   }
 }
-
-export const config = {
-  matcher: ["/api/:path*"],
-};
