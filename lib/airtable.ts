903c10d7bd95f9f9f37257b12f14caf43b8c4d99
Damian Bermudez <bermudezdamian7@gmail.com>
2023-01-18 19:09:41 -0300

    cambio en la base de airtable

--- lib/airtable.ts
+++ lib/airtable.ts
@@ -2,6 +2,7 @@ import Airtable from "airtable";
 var airtableBase = new Airtable({ apiKey: process.env.AIRTABLE_PRODUCTS_BASE }).base(
   process.env.AIRTABLE_BASE
 );
+console.log(airtableBase);
 
 export { airtableBase };
 
