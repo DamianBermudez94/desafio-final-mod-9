eecbcad4f3e868fdfa8c378f7aec3fa1e400b8b1
DamianBermudez94 <bermudezdamian7@gamil.com>
2025-05-15 22:36:57 -0300

    se agrego el archivo uploadController para poder subir las imagenes

--- lib/airtable.ts
+++ lib/airtable.ts
@@ -2,7 +2,6 @@ import Airtable from "airtable";
 var airtableBase = new Airtable({ apiKey: process.env.AIRTABLE_PRODUCTS_BASE }).base(
   process.env.AIRTABLE_BASE
 );
-console.log(airtableBase);
 
 export { airtableBase };
 
