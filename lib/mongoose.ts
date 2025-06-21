cf30435a9e2d61e299052b98a4200e5ecc8ebaf5
DamianBermudez94 <bermudezdamian7@gamil.com>
2025-06-21 00:10:40 -0300

    se creo el archivo para poder manejar las imagenes con la base de datos moongose

---
+++ lib/mongoose.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI || "";
+
+if (!MONGODB_URI) {
+  throw new Error("No está definida la variable MONGODB_URI");
+}
+
+export const connectDB = async () => {
+  try {
+    if (mongoose.connections[0].readyState) return;
+    await mongoose.connect(MONGODB_URI);
+    console.log("🔗 MongoDB conectado");
+  } catch (err) {
+    console.error("❌ Error conectando MongoDB:", err);
+  }
+};
