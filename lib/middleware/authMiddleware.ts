64f66f3b29c8a9e744e8595879e8929edac16fb6
DamianBermudez94 <bermudezdamian7@gamil.com>
2025-07-24 23:22:49 -0300

    se termino el middleware parea typar los datos de usuario

--- lib/middleware/authMiddleware.ts
+++ lib/middleware/authMiddleware.ts
@@ -1,31 +1,33 @@
 // lib/middlewares/authMiddleware.ts
 import type { NextApiRequest, NextApiResponse } from "next";
+import { AuthenticatedNextApiRequest, AuthTokenPayload } from "../../types/auth";
 import jwt from "jsonwebtoken";
-
 
 const SECRET = process.env.JWT_SECRET; // Asegurate de tenerlo en .env
 
-export async function adminAuth(req: NextApiRequest, res: NextApiResponse, next: Function) {
-    const authHeader = req.headers.authorization;
+export async function adminAuth(
+  req: AuthenticatedNextApiRequest,
+  res: NextApiResponse,
+  next: Function
+) {
+  const authHeader = req.headers.authorization;
 
-    if (!authHeader) {
-        return res.status(401).json({ message: "Token no proporcionado" });
+  if (!authHeader) {
+    return res.status(401).json({ message: "Token no proporcionado" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, SECRET) as AuthTokenPayload;
+
+    if (!decoded.isAdmin) {
+      return res.status(403).json({ message: "Acceso denegado: no sos admin" });
     }
 
-    const token = authHeader.split(" ")[1];
-
-    try {
-        const decoded: any = jwt.verify(token, SECRET);
-
-        if (!decoded.isAdmin) {
-            return res.status(403).json({ message: "Acceso denegado: no sos admin" });
-        }
-
-        // Lo agregamos al request para que lo use el endpoint si lo necesita
-        req.user = decoded;
-
-        next(); // Todo OK, seguimos
-    } catch (err) {
-        return res.status(401).json({ message: "Token inválido o expirado" });
-    }
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Token inválido o expirado" });
+  }
 }
